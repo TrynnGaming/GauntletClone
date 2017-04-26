7171620b754733dc4ef13e4542ab0a59a3127e11
Pat <pimpsound@yahoo.com>
2017-04-25 23:52:58 -0700

    Added code for Camera movement, player selection and instantiation based on selection of characters. Also updated the game control object script due to some issues it was having.

---
+++ Gauntlet Clone Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    /*-----------------------------------------------
+    DECLARATIONS
+    -----------------------------------------------*/
+    Vector3 tempPos;
+
+
+
+    /*-----------------------------------------------
+    START
+    -----------------------------------------------*/
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    /*-----------------------------------------------
+    UPDATE
+    -----------------------------------------------*/
+    // Update is called once per frame
+    void Update () {
+
+        //set camera position to update with position of CameraFollowObject
+
+        tempPos.z = CameraFollowObject.tempPos.z - 8; //need to update the value with a variable when possible
+        tempPos.x = CameraFollowObject.tempPos.x;
+        tempPos.y = 12;                               //update with variable when possible
+        transform.position = tempPos;
+	}
+}
--- Gauntlet Clone Project/Assets/Scripts/Character Scripts/Valkyrie.cs
+++ Gauntlet Clone Project/Assets/Scripts/Character Scripts/Valkyrie.cs
@@ -5,21 +5,75 @@ using UnityEngine;
 public class Valkyrie : BaseCharacter {
 
     /*-----------------------------------------------
-   DECLARATIONS
-   -----------------------------------------------*/
+     DECLARATIONS
+     -----------------------------------------------*/
 
     //BASE VALKYRIE VARIABLES
     private Vector3 startPos;
     private Vector3 tempPos;
 
+    //VARIABLE FOR CAMERA POS
+    public static Vector3 CameraPos;
 
+
+    /*-----------------------------------------------
+     START
+     -----------------------------------------------*/
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        startPos = transform.position;
+        tempPos = transform.position;
+    }
+
+    /*-----------------------------------------------
+     UPDATE
+     -----------------------------------------------*/
+    // Update is called once per frame
+    void Update()
+    {
+
+        //set CameraPos
+        CameraPos = transform.position;
+
+
+
+        //-----------------
+        //KEYBOARD CONTROLS
+        //-----------------
+
+        //MOVE LEFT
+        if (Input.GetKey("a"))
+        {
+            tempPos.x -= (moveSpeed * Time.deltaTime);
+
+        }
+
+        //MOVE RIGHT
+        if (Input.GetKey("d"))
+        {
+            tempPos.x += (moveSpeed * Time.deltaTime);
+        }
+
+        //MOVE UP
+        if (Input.GetKey("w"))
+        {
+            tempPos.z += (moveSpeed * Time.deltaTime);
+        }
+
+        //MOVE DOWN
+        if (Input.GetKey("s"))
+        {
+            tempPos.z -= (moveSpeed * Time.deltaTime);
+        }
+
+
+        //----------------
+        //GAMEPAD CONTROLS
+        //----------------
+
+
+        transform.position = tempPos;
+
+    }
 }
